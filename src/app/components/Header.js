6131894c8c48e688b24e1676724c9ee52bf200a3
ciaranRoche <ciaranroche@hotmail.co.uk>
2017-10-17 20:13:31 +0100

    encountered a bug with semantic and link menu, fixed it by passing link in as a tag in menu item

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -3,11 +3,19 @@ import {Menu, Segment, Header} from 'semantic-ui-react';
 import {Link} from 'react-router'
 
 class HeaderComponent extends Component{
+  state = {activeItem: 'home'}
+  
+  handleItemClick = (e, {name}) => this.setState({activeItem: name})
+
   render(){
+    const {activeItem} = this.state
     return(<div>
         <Menu pointing secondary>
-          <Link to='/'><Menu.Item name='Home'></Menu.Item></Link>
-          <Link to='about'><Menu.Item name='About'></Menu.Item></Link>
+          <Menu.Item as={Link} to='/' name='Home' active={activeItem === 'Home'} onClick={this.handleItemClick}/>
+          <Menu.Item as={Link} to='about' name='About' active={activeItem === 'About'} onClick={this.handleItemClick}/> 
+          <Menu.Menu position='right'>
+            <Menu.Item as={Link} to='/' name='Profile' active={activeItem === 'Profile'} onClick={this.handleItemClick}/>
+          </Menu.Menu>
         </Menu>
         <Header as='h1'>
           Rust
